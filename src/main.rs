7cadc7953bd53fdf779fe435d87ff3c954bd2285
Bradley Shawyer <21098019+bradleyess@users.noreply.github.com>
2020-09-22 18:45:05 +0930

    refactor: use lib.rs and export binary with main invocation of start_server

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-
-async fn say_hello(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("world");
-    return format!("Hello {name}", name = &name);
-}
+use payments::start_server;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(say_hello))
-            .route("/{name}", web::get().to(say_hello))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    return start_server()?.await;
 }
